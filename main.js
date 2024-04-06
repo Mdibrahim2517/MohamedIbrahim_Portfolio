092d41e3d9fa1f7f94fe829b79cbaa56c4fbae2e
Mdibrahim2517 <s.mohamed.ibrahim890@gmail.com>
2024-04-06 21:55:59 +0530

    add

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // alert("hello");
 // import 'animate.css'
-var typed= new Typed(".text",{
-    strings:["Entrepreneur ✌","FullStack Developer ❤","Gamer 😎"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop:true
+var typed = new Typed(".text", {
+  strings: ["Entrepreneur ✌", "FullStack Developer ❤", "Gamer 😎"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
 });
